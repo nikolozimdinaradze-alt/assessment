825d0593f3142391f0bd09dc0472c9635d8b1859
Nika Mdinaradze <nikolozi_mdinaradze@epam.com>
2026-06-08 12:25:04 +0400

    chore: add version tracking

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from contextlib import asynccontextmanager
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.websockets import WebSocket, WebSocketDisconnect
 
+from src import __version__
 from src.chatbot import QAChatbot
 from src.conf import settings
 from src.database import apply_migrations, get_db
@@ -27,6 +28,13 @@ app = FastAPI(lifespan=lifespan)
 @app.get("/health-check")
 async def health_check():
     return {"status": "ok"}
+
+
+@app.get("/version")
+async def version():
+    return {
+        "version": __version__,
+    }
 
 
 @app.get("/chat-session", response_model=list[ChatSession])
